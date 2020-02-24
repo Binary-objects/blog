dba4bd3c4ca257a104fdced251393e79f1ccdead
vikas <vikas.patel@spluspl.com>
2020-02-24 16:29:12 +0530

    concept of service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Billing\BankPaymentGateway;
+use App\Billing\CreditPaymentGateway;
+use App\Billing\PaymentGatewayContract;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -17,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->singleton(PaymentGatewayContract::class, function ($app){
+
+            if(request()->has('credit')){
+                return new CreditPaymentGateway('INR');
+            }
+
+            return new BankPaymentGateway('usd');
+
+        });
     }
 
     /**
@@ -28,16 +40,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        //
-        $this->app->singleton(PaymentGatewayContract::class, function ($app){
-
-            if(request()->has('credit')){
-                return new CreditPaymentGateway('INR');
-            }
-
-            return new BankPaymentGateway('usd');
-
-        });
 
     }
 }
